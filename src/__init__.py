b917ae006a9904baa575212e8c5fdbe14542b4d7
Josh Poplawski <97743407+APLjoshPop@users.noreply.github.com>
2023-03-03 13:25:38 -0500

    set up environment

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,4 @@
-#__init__.py is a convention to tell python this is a python package. Usually these are empty, there are rare cases code  is in here.
-#refered to as "dunder" just meaining it has special significance for python, such as convention
+# __init__.py is a convention to tell python this is a python package.
+# Usually these are empty, there are rare cases code  is in here.
+# refered to as "dunder" just meaining it has special significance for python,
+# such as conventions
